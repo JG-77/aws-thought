196efc1d45e7b1e156bd7300d278ef52df433c2a
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-09-09 19:21:52 -0700

    created three APi enpoints for app thoughts

--- starter/server/routes/user-routes.js
+++ starter/server/routes/user-routes.js
@@ -29,7 +29,75 @@ router.get('/users', (req, res) => {
       res.json(data.Items);
     }
   });
-  // more to come . . .
 });
+
+//use query parameters to pass the username from the client to the server
+router.get('/users/:username', (req, res) => {
+  //capture the query parameter with the req.params object
+  console.log(`Querying for thought(s) from ${req.params.username}.`);
+
+  const params = {
+    TableName: table,
+    //KeyConditionExpression property specifies the search criteria
+    //#un represents the attribute name username --> defined in the ExpressionAttributeNames property
+    KeyConditionExpression: '#un = :user',
+    // # or : used a prefix to avoid using a list of reserved words from DynamoDB that can't be used as attribute names
+    ExpressionAttributeNames: {
+      '#un': 'username',
+      '#ca': 'createdAt',
+      '#th': 'thought',
+    },
+    //ExpressionAttributeValues property is assigned to req.params.username, which was received from the client
+    ExpressionAttributeValues: {
+      //use the username selected by the user in the client to determine the condition of the search
+      ':user': req.params.username,
+    },
+    //determines which attributes or columns will be returned
+    ProjectionExpression: '#th, #ca',
+    //This property takes a Boolean value
+    //default setting is true, which specifies the order for the sort key, which will be ascending
+    //false so that the order is descending
+    ScanIndexForward: false,
+  };
+
+  //use the service interface object, dynamodb, and the query method to retrieve the user's thoughts from the database
+  dynamodb.query(params, (err, data) => {
+    if (err) {
+      console.error('Unable to query. Error:', JSON.stringify(err, null, 2));
+      res.status(500).json(err); // an error occurred
+    } else {
+      console.log('Query succeeded.');
+      res.json(data.Items);
+    }
+  });
+}); // closes the route for router.get(users/:username)
+
+// Create new user at /api/users
+router.post('/users', (req, res) => {
+  //set the params object to the form data of the ThoughtForm, which we can access with req.body
+  const params = {
+    TableName: table,
+    Item: {
+      username: req.body.username,
+      //use the JavaScript native Date object to set the value of the createdAt property
+      createdAt: Date.now(),
+      thought: req.body.thought,
+    },
+  };
+
+  // database call
+  dynamodb.put(params, (err, data) => {
+    if (err) {
+      console.error(
+        'Unable to add item. Error JSON:',
+        JSON.stringify(err, null, 2)
+      );
+      res.status(500).json(err); // an error occurred
+    } else {
+      console.log('Added item:', JSON.stringify(data, null, 2));
+      res.json({ Added: JSON.stringify(data, null, 2) });
+    }
+  });
+}); // ends the route for router.post('/users')
 
 module.exports = router;
