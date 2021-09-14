09c6d124942379ad035466c77052ad69c06bbf7c
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-09-13 17:28:46 -0700

    home.js changes for use state

--- starter/client/src/pages/Home.js
+++ starter/client/src/pages/Home.js
@@ -6,6 +6,28 @@ const Home = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [thoughts, setThoughts] = useState([]);
 
+  //useEffect hook to request all the users' thoughts to render on the component mount event
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        //GET method to fetch the data from the database
+        const res = await fetch('/api/users');
+        //store as a JSON into the data constant
+        const jsonData = await res.json();
+        // sort the array by createdAt property ordered by descending values
+        const data = jsonData.sort((a, b) =>
+          a.createdAt < b.createdAt ? 1 : -1
+        );
+        // store the data using the useState setter method, setThoughts
+        setThoughts([...data]);
+        setIsLoaded(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <main>
       <div className="flex-row justify-space-between">
@@ -16,8 +38,12 @@ const Home = () => {
           {!isLoaded ? (
             <div>Loading...</div>
           ) : (
-              <ThoughtList thoughts={thoughts} setThoughts={setThoughts} title="Some Feed for Thought(s)..." />
-            )}
+            <ThoughtList
+              thoughts={thoughts}
+              setThoughts={setThoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )}
         </div>
       </div>
     </main>
