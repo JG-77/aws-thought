945d5b05508015d66a1515afabb57ba6ee9e4509
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-09-20 18:47:13 -0700

    ran npm build

---
+++ starter/client/build/precache-manifest.6639e56cc727f7b952eca142844a7bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2213ec37703d0cd52aa93d78c83f2f13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57a830b93127696f9272",
+    "url": "/static/css/main.c5ae7a51.chunk.css"
+  },
+  {
+    "revision": "80e22bedb8b254051a0f",
+    "url": "/static/js/2.4db26def.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4db26def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a830b93127696f9272",
+    "url": "/static/js/main.ece018cc.chunk.js"
+  },
+  {
+    "revision": "c71e5690aa6c32254dd7",
+    "url": "/static/js/runtime-main.758ba164.js"
+  }
+]);
